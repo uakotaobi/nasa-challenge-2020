a1a3c6d1d993ed3a568fa9bf7c65157664c4c707
the-cooler-jackson <jchartier_23@esusdstudents.org>
2020-10-17 13:57:32 -0700

    Included SDL and initialized it.

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include "SDL.h"
 
 int main() {
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+        return 1;
+	
+	}
+	SDL_Quit();
+		
+    
     std::cout << "Program finished.\n";
 }
