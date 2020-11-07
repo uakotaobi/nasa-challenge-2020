13b46caf316a4ba639176315303aa3eb1ace2c00
Xmassub <mbusse_24@esusdstudents.org>
2020-11-07 14:33:27 -0800

    Added text to info view
    - Used format similar to button view to use render text and gidole font to draw text on info view

--- src/info_view.cpp
+++ src/info_view.cpp
@@ -3,11 +3,27 @@
 #include "SDL.h"
 
 InfoView::InfoView(SDL_Rect infoBoundary, int deltaX, int deltaY) {
+    std::string infoText = "Info";
     boundaryInfoView = infoBoundary;
+    const AssetManager& am = getAssetManager();
+    TTF_Font* infoFont = am.getFont("gidole");
+    renderText = TTF_RenderUTF8_Shaded(infoFont, infoText.c_str(), SDL_Color{255, 255, 255, 255}, SDL_Color{255, 61, 64});
 }
 
 void InfoView::draw(SDL_Surface* screen) {
     SDL_FillRect(screen, &boundaryInfoView, SDL_MapRGB(screen->format, 255, 61, 64));
+
+    // Draw the info text.
+    SDL_Rect rec = SDL_Rect{
+      boundaryInfoView.x + boundaryInfoView.w / 2 - renderText->w / 2 ,
+      boundaryInfoView.y + boundaryInfoView.h / 8 - renderText->h / 2,
+      0 /* ignored */,
+      0 /* ignored */
+    };
+    SDL_BlitSurface(renderText,
+                    nullptr,
+                    screen,
+                    &rec);
 }
 
 SDL_Rect InfoView::boundary() const {
--- src/info_view.h
+++ src/info_view.h
@@ -1,7 +1,10 @@
 #ifndef INFO_VIEW_H_INCLUDED
 #define INFO_VIEW_H_INCLUDED
 
+#include <SDL.h>
 #include "view.h"
+#include <string>
+#include <functional>
 
 class InfoView : public View {
     public:
@@ -11,6 +14,8 @@ class InfoView : public View {
         // void handleResize(SDL_Surface* screen);
     private:
         SDL_Rect boundaryInfoView;
+        std::string infoText_;
+        SDL_Surface* renderText;
 };
 
 
