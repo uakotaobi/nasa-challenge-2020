fa09dcaa53900c77723daa7abe655ac329e84042
Xmassub <mbusse_24@esusdstudents.org>
2020-11-15 17:01:57 -0800

    Fixed matrix multiplication for loop logic
    - Replaced wrong sauce with the sauce of goodness and directness
    
    - Last time, we made a matrix multiplication function that we thought was a "clever hack" but infact it was completely broken
    - Fixed it by looking through for loops and realizing that we ran the j loop 16 times everytime we ran 1 i loop
    - Also added m1_row, m2_column, m1_data_index, and m2_data_index to make logic easier to understand

--- src/matrix.cpp
+++ src/matrix.cpp
@@ -1,7 +1,11 @@
 #include "matrix.h"
+#include <iostream>
 
 Matrix::Matrix() {
-
+    data = {0, 0, 0, 0,
+            0, 0, 0, 0,
+            0, 0, 0, 0,
+            0, 0, 0, 0};
 }
 
 
@@ -16,30 +20,42 @@ Matrix::Matrix(double a11, double a12, double a13, double a14,
         a31, a32, a33, a34,
         a41, a42, a43, a44
     };
-    
+
 }
 
 Matrix operator*(Matrix m1, Matrix m2) {
     Matrix result;
-    // "k" is the current position in the result array.  
+    std::cout << result;
+    // FOR k from 0 to 15
+    //     m1_row = FLOOR(k/4)
+    //     m2_column = k % 4
+    //     FOR i from 0 to 3
+    //         result[k] += m1[m1_row * 4 + i] * m2[m2_column + 4 * i]
+    //     NEXT i
+    // NEXT k
+
+    // "k" is the current position in the result array.
     for (int k = 0; k < 16; k += 1) {
-        // "i" is the current row of m1.
+        int m1_row = int(k/4);
+        int m2_column = k%4;
+
+        // "i" is the current position in the row and colunm we are multiplying
         for (int i = 0; i < 4; i += 1) {
-            // "j" is both the current row of m2 and current column of m1
-            // The current column of m2 is k MOD 4.
-            for (int j = 0; j < 4; j += 1) {
-                result.data[k] += m1.data[4 * i + j] * m2.data[4 * j + (k % 4)];
-            }
+            int m1_data_index = m1_row * 4 + i;
+            int m2_data_index = m2_column + 4 * i;
+            result.data[k] += m1.data[m1_data_index] * m2.data[m2_data_index];
         }
-    }  
-    return result; 
+    }
+    return result;
 }
 
 std::ostream& operator<<(std::ostream& s, Matrix m) {
     for (int i = 0; i < 16; i += 1) {
-        s << m.data[i] << ", ";        
+        if ((i + 1) % 4 == 0) {
+            s << m.data[i] << ", \n";
+        } else {
+            s << m.data[i] << ", ";
+        }
     }
     return s;
-}     
-    
-    
+}
