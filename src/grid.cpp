ad6274315b7b1b374295115bc0adcdcf82d71fc1
the-cooler-jackson <jchartier_23@esusdstudents.org>
2020-12-12 14:43:53 -0800

    Added the grid cells and default constructor.
      Or in other words the coordinates.

--- src/grid.cpp
+++ src/grid.cpp
@@ -3,3 +3,14 @@
 GridPoint::GridPoint() : Point(0, 0, 0), color(SDL_Color{0, 0, 0}), temperatureKelvin(0), slopeDeg(0) {}
 GridPoint::GridPoint(Point p, SDL_Color color_, double temperatureKelvin_, double slopeDeg_)
     : Point(p), color(color_), temperatureKelvin(temperatureKelvin_), slopeDeg(slopeDeg_) {}
+
+
+Grid::Grid() : lattice((49 + 1) * (49 + 1)), 
+               center(0, 0, 0), 
+               axisX(1, 0, 0), 
+               axisY(0, 1, 0),
+               axisZ(0, 0, 1),
+               rows(49),
+               columns(49),
+               cellSize(1) {}
+               
--- src/grid.h
+++ src/grid.h
@@ -4,6 +4,7 @@
 #include <vector>
 #include "SDL.h"
 #include "point.h"
+#include "vector.h"
 
 struct GridPoint : public Point {
     // We don't need elevation variable, we have this->y
@@ -18,9 +19,21 @@ struct GridPoint : public Point {
 class Grid {
     public:
         Grid();
-        Grid(int rows, int columns, double defaultDistance);
+        
+        // Construct a grid with the given dimensions.
+        // 
+        // - rows: the depth of the grid 
+        // - columns: the width of the grid
+        // - cellSize: the space between grid cells according to the grid's own basis
+        Grid(int rows, int columns, double cellSize = 1.0 );
     private:
         std::vector<GridPoint> lattice;
+        Point center;
+        Vector axisX, axisY, axisZ;
+        int rows, columns;
+        double cellSize;
+        
+        
 };
 
 
